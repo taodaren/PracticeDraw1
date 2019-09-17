f84091d8ef81b3af2c1203a0690b9b907100d86f
taodaren <taodaren1125@gmail.com>
2019-09-17 08:53:25 +0800

    drawRect()

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice03DrawRectView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice03DrawRectView.java
@@ -2,21 +2,24 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
-public class Practice3DrawRectView extends View {
+public class Practice03DrawRectView extends View {
 
-    public Practice3DrawRectView(Context context) {
+    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+    public Practice03DrawRectView(Context context) {
         super(context);
     }
 
-    public Practice3DrawRectView(Context context, @Nullable AttributeSet attrs) {
+    public Practice03DrawRectView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public Practice3DrawRectView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public Practice03DrawRectView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
@@ -25,5 +28,9 @@ public class Practice3DrawRectView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawRect() 方法画矩形
+
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawRect(340, 200, 740, 600, paint);
+
     }
 }
